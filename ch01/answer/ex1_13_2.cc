71f0653f7d11bd1625a81073137b0202aefd11c0
gycg <1765479220@qq.com>
2016-09-23 17:13:15 +0800

    del

--- ch01/answer/ex1_13_2.cc
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-int main()
-{
-    for (int val = 10; val >= 0; --val)
-        std::cout << val << std::endl;
-    return 0;
-}
--- ch01/answer/ex1_20.cc
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "Sales_item.h"
-int main()
-{
-    Sales_item item;
-    while(std::cin >> item)
-        std::cout << item << std::endl;
-}
